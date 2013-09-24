18d0f91a13a8ad6d11653cf0d4f35cea56fdf7cb
Tom Copeland <tom.copeland@livingsocial.com>
2013-09-24 15:56:46 -0400

    A working finder

--- Gemfile
+++ Gemfile
@@ -1,3 +1,3 @@
 source "https://rubygems.org"
 
-#gem 'byebug', :path => "/Users/tomcopeland/github.com/byebug"
+gem 'byebug', :path => "/Users/tomcopeland/github.com/byebug"
--- tracer.rb
+++ tracer.rb
@@ -1,23 +1,83 @@
-
 require 'rubygems'
 require 'bundler'
 
 Bundler.require
 
+class SelectFollowedByFirstFinder
+  def initialize
+    @result_of_select_invocation = nil
+  end  
+  def register_tracepoint
+    TracePoint.trace(:c_call, :c_return) do |tp|
+      next unless tp.defined_class == Array && (tp.method_id == :select || tp.method_id == :first)
+      
+      # grab the result of the select call 
+      if tp.method_id == :select && tp.event == :c_return # TODO also check for msg receiver of Enumerable
+        @result_of_select_invocation = tp.return_value
+      end
+    
+      # was this the object we called select on?  if so, are we calling #first?
+      # if any methods were invoked in the meantime, don't check this
+      if tp.event == :c_call && @result_of_select_invocation.object_id == tp.self.object_id && tp.method_id == :first
+        puts "GOT ONE"
+      end
+    end
+  end
+end
+
 def foo
-  bar = [1,2,3]
-  baz = bar.select {|x| x > 1}
-  baz.first
+  x = [1,2,3].select {|x| x > 1}
+  [1,2].first
 end
 
-def checkit(tp)
-  return unless tp.defined_class == Array && tp.method_id == :select
-  # puts tp.methods.sort - Object.methods.sort
+def foo2
+  [1,2,3].select {|x| x > 1}.first
 end
 
-TracePoint.trace(:c_call) do |tp|
-  checkit(tp)
-end
-
+f = SelectFollowedByFirstFinder.new
+f.register_tracepoint
+puts "should not find one"
+foo
+puts "should find one"
+foo2
+puts "should not find one"
 foo
 
+######
+# @result_of_select_invocation = nil
+# 
+# TracePoint.trace(:c_call, :c_return) do |tp|
+#   next unless tp.defined_class == Array && (tp.method_id == :select || tp.method_id == :first)
+#   
+#   # grab the result of the select call 
+#   if tp.method_id == :select && tp.event == :c_return # TODO also check for msg receiver of Enumerable
+#     @result_of_select_invocation = tp.return_value
+#   end
+# 
+#   # was this the object we called select on?  if so, are we calling #first?
+#   # if any methods were invoked in the meantime, don't check this
+#   if tp.event == :c_call && @result_of_select_invocation.object_id == tp.self.object_id && tp.method_id == :first
+#     puts "GOT ONE"
+#   end
+#     
+#   # puts "#{tp.event} - method_id is #{tp.method_id}"
+#   # puts "tp.self.object_id is #{tp.self.object_id}"
+# end
+######
+
+# 
+# foo2
+
+# TracePoint.trace(:c_return) do |tp|
+#   next unless tp.defined_class == Array && (tp.method_id == :select || tp.method_id == :first)
+#   puts "RETURN"
+#   puts "tp.self.object_id is #{tp.self.object_id}"
+#   puts "return_value is #{tp.return_value}"
+#   puts "return_value object id is #{tp.return_value.object_id}"
+# end
+
+# TracePoint.trace do |tp|
+#   next if tp.method_id == :puts || tp.method_id == :write
+#   warn "line %4s %-8s %-11p" % [tp.lineno, tp.event, tp.method_id] 
+# end
+# 
