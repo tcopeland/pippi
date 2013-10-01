78eea9467ea916eaf0425d66f8e6e9d2cd50bac5
Tom Copeland <tom.copeland@livingsocial.com>
2013-10-01 19:39:26 -0400

    And still more test refactoring

--- test/unit/select_followed_by_first_test.rb
+++ test/unit/select_followed_by_first_test.rb
@@ -4,15 +4,15 @@ require 'fileutils'
 class SelectFollowedByFirstTest < MiniTest::Test
 
   def test_canonical_case_is_found
-    assert_equal 1, execute_pippi_on(foo_bar_code_sample("[1,2,3].select {|x| x > 2 }.first"), "SelectFollowedByFirst").size
+    assert_equal 1, execute_pippi_on(foo_bar_code_sample("[1,2,3].select {|x| x > 2 }.first")).size
   end
 
   def test_two_line_case_is_found
-    assert_equal 1, execute_pippi_on(foo_bar_code_sample("x = [1,2,3].select {|x| x > 2 } ; x.first"), "SelectFollowedByFirst").size
+    assert_equal 1, execute_pippi_on(foo_bar_code_sample("x = [1,2,3].select {|x| x > 2 } ; x.first")).size
   end
 
   def test_rule_does_not_fire_if_no_first_call
-    assert execute_pippi_on(foo_bar_code_sample("[1,2,3].select {|x| x > 2 }"), "SelectFollowedByFirst").empty?
+    assert execute_pippi_on(foo_bar_code_sample("[1,2,3].select {|x| x > 2 }")).empty?
   end
 
   private
@@ -21,13 +21,17 @@ class SelectFollowedByFirstTest < MiniTest::Test
     CodeSample.new.tap {|c| c.code_text = "class Foo ; def bar ; #{code} ; end ; end" ; c.eval_to_execute = "Foo.new.bar" }
   end
 
-  def execute_pippi_on(code, rule)
+  def execute_pippi_on(code)
     File.open(tmp_code_sample_file_name, "w") {|f| f.syswrite(code.code_text) }
-    cmd = "bundle exec ruby bin/pippi #{tmp_code_sample_file_name} #{rule} #{code.eval_to_execute} #{output_file_name}"
+    cmd = "bundle exec ruby bin/pippi #{tmp_code_sample_file_name} #{rule_for_test} #{code.eval_to_execute} #{output_file_name}"
     IO.popen(cmd).close
     report = File.read(output_file_name).split("\n")
     FileUtils.rm_f(output_file_name) if File.exists?(output_file_name)
     report
+  end
+
+  def rule_for_test
+    "SelectFollowedByFirst"
   end
 
   def output_file_name
