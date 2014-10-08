48f0e3434804535586b832afcf6109841c4bb145
Tom Copeland <tom.copeland@livingsocial.com>
2014-10-08 18:47:33 -0400

    Fixed autorunner

--- lib/pippi/auto_runner.rb
+++ lib/pippi/auto_runner.rb
@@ -1,10 +1,22 @@
 module Pippi
 
   class AutoRunner
+    attr_reader :ctx
+
     def initialize
-      ctx = Pippi::Context.new(:report => Pippi::Report.new(Logger.new("log/pippi.log", "w")), :logger => self)
-      Pippi::CheckLoader.new(ctx, "basic").checks.each(&:decorate)
+      @ctx = Pippi::Context.new(:report => Pippi::Report.new(Logger.new("log/pippi.log", "w")), :logger => self)
+      Pippi::CheckLoader.new(@ctx, "basic").checks.each(&:decorate)
+      at_exit { dump }
     end
+
+    def dump
+      File.open("log/pippi.log", "w") do |outfile|
+        @ctx.report.problems.each do |problem|
+          outfile.syswrite("#{problem.to_text}\n")
+        end
+      end
+    end
+
   end
 
 end
