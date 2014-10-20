fb4a98b9de396148c4dae6ab04d15e097562b1d5
Tom Copeland <tom.copeland@livingsocial.com>
2014-10-20 07:43:49 -0400

    Removing Rails cache hack
    I think this is no longer necessary now that I'm properly removing
    the watcher methods.

--- lib/pippi/auto_runner.rb
+++ lib/pippi/auto_runner.rb
@@ -8,22 +8,12 @@ module Pippi
       @ctx = Pippi::Context.new
       Pippi::CheckLoader.new(@ctx, checkset).checks.each(&:decorate)
       at_exit { dump }
-      maybe_disable_rails_caching
     end
 
     def dump
       File.open("log/pippi.log", "w") do |outfile|
         @ctx.report.problems.each do |problem|
           outfile.syswrite("#{problem.to_text}\n")
-        end
-      end
-    end
-
-    def maybe_disable_rails_caching
-      if defined?(ActiveSupport::Cache::Store)
-        ActiveSupport::Cache::Store.class_eval do
-          def write(*_)
-          end
         end
       end
     end
