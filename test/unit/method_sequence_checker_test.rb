131c5286f6cff385a76f40c60b4c531027431fe8
Tom Copeland <tom.copeland@livingsocial.com>
2014-12-12 09:26:16 -0500

    Revert "Backfilling tests"
    This reverts commit 4ed610727afed6693f5b319e89fdae9a6c1773c8.
    
    Oops, this doesn't work on Ruby 2.0.0, thanks TravisCI!

--- test/unit/method_sequence_checker_test.rb
+++ test/unit/method_sequence_checker_test.rb
@@ -5,19 +5,10 @@ class MethodSequenceCheckerTest < MiniTest::Test
   class TestCheck < Pippi::Checks::Check
   end
 
-  def test_decorate_should_add_accessor_to_decorated_class
+  def test_decorate
     check = TestCheck.new(nil)
     m = MethodSequenceChecker.new(check, Array, "select", "size", MethodSequenceChecker::ARITY_TYPE_BLOCK_ARG, MethodSequenceChecker::ARITY_TYPE_NONE, false)
     m.decorate
-    assert Array._pippi_check_testcheck.kind_of?(TestCheck)
-  end
-
-  def test_decorate_should_add_a_module_that_decorates_the_first_method
-    check = TestCheck.new(nil)
-    m = MethodSequenceChecker.new(check, Array, "select", "size", MethodSequenceChecker::ARITY_TYPE_BLOCK_ARG, MethodSequenceChecker::ARITY_TYPE_NONE, false)
-    assert_equal Array.ancestors[0], Array
-    m.decorate
-    assert Array.ancestors[0] != Array
   end
 
 end
