188815de8553b8643992ea94fe32ef0cf72e322e
Tom Copeland <tom.copeland@livingsocial.com>
2014-12-15 07:54:56 -0500

    Revert "Revert "Backfilling tests""
    This reverts commit 131c5286f6cff385a76f40c60b4c531027431fe8.
    
    This was not a Ruby 2.0 incompatibility.  Instead I was not accounting
    for the fact that these tests were mutating classes which were not
    reloaded between tests, and the failure only occurred on certain
    orderings.  So now each test operates on a different class.

--- test/unit/method_sequence_checker_test.rb
+++ test/unit/method_sequence_checker_test.rb
@@ -5,10 +5,36 @@ class MethodSequenceCheckerTest < MiniTest::Test
   class TestCheck < Pippi::Checks::Check
   end
 
-  def test_decorate
+  class SomeClass
+    def select(&blk)
+      []
+    end
+    def size
+      0
+    end
+  end
+  class SomeClass2
+    def select(&blk)
+      []
+    end
+    def size
+      0
+    end
+  end
+
+  def test_decorate_should_add_accessor_to_decorated_class
     check = TestCheck.new(nil)
-    m = MethodSequenceChecker.new(check, Array, "select", "size", MethodSequenceChecker::ARITY_TYPE_BLOCK_ARG, MethodSequenceChecker::ARITY_TYPE_NONE, false)
+    m = MethodSequenceChecker.new(check, SomeClass2, "select", "size", MethodSequenceChecker::ARITY_TYPE_BLOCK_ARG, MethodSequenceChecker::ARITY_TYPE_NONE, false)
     m.decorate
+    assert SomeClass2._pippi_check_testcheck.kind_of?(TestCheck)
+  end
+
+  def test_decorate_should_add_a_module_that_decorates_the_first_method
+    check = TestCheck.new(nil)
+    m = MethodSequenceChecker.new(check, SomeClass, "select", "size", MethodSequenceChecker::ARITY_TYPE_BLOCK_ARG, MethodSequenceChecker::ARITY_TYPE_NONE, false)
+    assert_equal SomeClass.ancestors[0], SomeClass
+    m.decorate
+    assert SomeClass.ancestors[0] != SomeClass
   end
 
 end
