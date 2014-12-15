1bc4b5d26472425b8845834841a321cb15b948f9
Tom Copeland <tom.copeland@livingsocial.com>
2014-12-15 18:25:08 -0500

    Backfilling another test

--- test/unit/method_sequence_checker_test.rb
+++ test/unit/method_sequence_checker_test.rb
@@ -6,35 +6,46 @@ class MethodSequenceCheckerTest < MiniTest::Test
   end
 
   class SomeClass
-    def select(&blk)
-      []
-    end
-    def size
-      0
-    end
+    def select(&blk) ; [] ; end
+    def size ; 0 ; end
   end
   class SomeClass2
-    def select(&blk)
-      []
-    end
+    def select(&blk) ; [] ; end
+    def size ; 0 ; end
+  end
+  class SomeClass3
+    def select(&blk) ; [] ; end
+    def size ; 0 ; end
+  end
+
+  module MyModule
     def size
-      0
+      raise "boom"
     end
   end
 
   def test_decorate_should_add_accessor_to_decorated_class
     check = TestCheck.new(nil)
-    m = MethodSequenceChecker.new(check, SomeClass2, "select", "size", MethodSequenceChecker::ARITY_TYPE_BLOCK_ARG, MethodSequenceChecker::ARITY_TYPE_NONE, false)
+    m = MethodSequenceChecker.new(check, SomeClass, "select", "size", MethodSequenceChecker::ARITY_TYPE_BLOCK_ARG, MethodSequenceChecker::ARITY_TYPE_NONE, false)
     m.decorate
-    assert SomeClass2._pippi_check_testcheck.kind_of?(TestCheck)
+    assert SomeClass._pippi_check_testcheck.kind_of?(TestCheck)
   end
 
   def test_decorate_should_add_a_module_that_decorates_the_first_method
     check = TestCheck.new(nil)
-    m = MethodSequenceChecker.new(check, SomeClass, "select", "size", MethodSequenceChecker::ARITY_TYPE_BLOCK_ARG, MethodSequenceChecker::ARITY_TYPE_NONE, false)
-    assert_equal SomeClass.ancestors[0], SomeClass
+    m = MethodSequenceChecker.new(check, SomeClass2, "select", "size", MethodSequenceChecker::ARITY_TYPE_BLOCK_ARG, MethodSequenceChecker::ARITY_TYPE_NONE, false)
+    assert_equal SomeClass2.ancestors[0], SomeClass2
     m.decorate
-    assert SomeClass.ancestors[0] != SomeClass
+    assert SomeClass2.ancestors[0] != SomeClass2
+  end
+
+  def test_accept_a_module_in_place_of_the_second_arity_type
+    check = TestCheck.new(nil)
+    m = MethodSequenceChecker.new(check, SomeClass3, "select", "size", MethodSequenceChecker::ARITY_TYPE_BLOCK_ARG, MyModule, false)
+    m.decorate
+    assert_equal("boom", assert_raises(RuntimeError) do
+      SomeClass3.new.select {|x| }.size
+    end.message)
   end
 
 end
